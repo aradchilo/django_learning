3a840ddec019bb2d9d971cc98527df9da9fa3427
aradchilo <aradchilo@gmail.com>
2023-02-02 17:27:35 +0400

    changing classes to class-based views

--- notes/views.py
+++ notes/views.py
@@ -4,17 +4,15 @@ from .models import Notes
 
 from django.http import Http404
 
-from django.views.generic import ListView
+from django.views.generic import DetailView, ListView
 
 
 class NotesListView(ListView):
     model = Notes
     context_object_name = "notes"
+    template_name = "notes/notes_list.html"
 
-
-def detail(request, pk):
-    try:
-        note = Notes.objects.get(pk=pk)
-    except Notes.DoesNotExist:
-        raise Http404("Note does not exists")
-    return render(request, 'notes/notes_detail.html', {'note': note})
+class NotesDetailView(DetailView):
+    model = Notes
+    context_object_name = "note"
+    template_name = "notes/notes_detail.html"
