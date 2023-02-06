2189275f1b756e4e12643c95a60dff6fd2d3fdcd
aradchilo <aradchilo@gmail.com>
2023-02-06 20:39:56 +0400

    work with forms and CRUD (POST, UPDATE, DELETE)

--- notes/views.py
+++ notes/views.py
@@ -1,16 +1,37 @@
+from urllib import request
+
 from django.shortcuts import render
 
+from .forms import NotesForm
 from .models import Notes
 
-from django.http import Http404
+from django.views.generic import DetailView, ListView, CreateView, UpdateView
+from django.views.generic.edit import DeleteView
 
-from django.views.generic import DetailView, ListView
+
+class NotesDeleteView(DeleteView):
+    model = Notes
+    success_url = '/ssmart/notes'
+    template_name = 'notes/notes_delete.html'
+
+
+class NotesUpdateView(UpdateView):
+    model = Notes
+    success_url = '/smart/notes'
+    form_class = NotesForm
+
+
+class NotesCreateView(CreateView):
+    model = Notes
+    success_url = '/smart/notes'
+    form_class = NotesForm
 
 
 class NotesListView(ListView):
     model = Notes
     context_object_name = "notes"
     template_name = "notes/notes_list.html"
+
 
 class NotesDetailView(DetailView):
     model = Notes
